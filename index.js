9d67a5033c628d68dac07cb1334bb594bc7a2977
Zanti01121995 <santhoshsekar788@gmail.com>
2023-12-03 00:17:54 +0530

    mmm

--- index.js
+++ index.js
@@ -5,18 +5,23 @@ const timestamp = new Date().toDateString();
 const text = `this is the content which includes the timestamp data ==>> ${timestamp} as the file name`
 
 console.log("timestamp", timestamp);
-fs.writeFile(`./contents/sample_${timestamp}.txt`, text, (err) => {
-    if (err) throw err;
-    console.log(`Completed writing sample_${timestamp}.txt`);
-});
 
-fs.readdir('./contents', "utf-8", (err, data) => {
-    if (err) throw err;
-    console.log("readsed", data);
-})
 
-app.get('/', function (req, res) {
+//////api to write the file inside the folder////////////
+app.get('/write', function (req, res) {
     res.send('Hello World')
+    fs.writeFile(`./contents/sample_${timestamp}.txt`, text, (err) => {
+        if (err) throw err;
+        console.log(`Completed writing sample_${timestamp}.txt`);
+    });
+})
+//////api to read the filename inside the folder////////////
+app.get('/read', (req, res) => {
+    fs.readdir('./contents', "utf-8", (err, data) => {
+        if (err) throw err;
+        console.log("read", data);
+    })
+
 })
 
 app.listen(5000, () => { console.log('listening on port 5000') })
